ede92d495abc03a452d987e9d9260970d756f9c8
Rostyslav Lysenko <lysenkor@digicode.com>
2022-05-16 13:08:57 +0300

    changed MovieInfoResponseDto

--- src/movie/dto/movie.Info.Response.dto.ts
+++ src/movie/dto/movie.Info.Response.dto.ts
@@ -1,28 +1,19 @@
-import { IsString } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class MovieInfoResponseDto {
+
   @IsString()
   readonly Title: string;
-  @IsString()
-  readonly Year: string;
-  @IsString()
-  readonly Released: string;
+
+  @IsNumber()
+  readonly YearReleased: number;
+
   @IsString()
   readonly Runtime: string;
+
   @IsString()
   readonly Genre: string;
-  @IsString()
-  readonly Director: string;
-  @IsString()
-  readonly Actors: string;
-  @IsString()
-  readonly Plot: string;
-  @IsString()
-  readonly Language: string;
-  @IsString()
-  readonly Country: string;
-  @IsString()
-  readonly Poster: string;
-  @IsString()
-  readonly imdbRating: string;
+
+  @IsNumber()
+  readonly ImdbRating: number;
 }
