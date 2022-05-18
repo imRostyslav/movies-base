74c37db59267227e644af29752f33ae6081e4c0c
Rostyslav Lysenko <lysenkor@digicode.com>
2022-05-18 15:06:14 +0300

    deleted unused import

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Query, Req } from '@nestjs/common';
 import { GetMovieInfoDto } from './dto/get.Movie.Info.dto';
-import { MovieInfoResponseDto } from './dto/movie.Info.Response.dto';
 import { MovieService } from './movie.service';
 
 
