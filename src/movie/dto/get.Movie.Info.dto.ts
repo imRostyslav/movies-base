821f119d8893e160431064f5ea3630396c32e8a1
Rostyslav Lysenko <lysenkor@digicode.com>
2022-05-19 18:30:00 +0300

    add api property

--- src/movie/dto/get.Movie.Info.dto.ts
+++ src/movie/dto/get.Movie.Info.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class GetMovieInfoDto {
+	@ApiProperty()
 	@IsString()
 	readonly title: string;
 }
--- src/movie/dto/movie.Info.Response.dto.ts
+++ src/movie/dto/movie.Info.Response.dto.ts
@@ -1,19 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString } from 'class-validator';
 
 export class MovieInfoResponseDto {
 
+  @ApiProperty()
   @IsString()
   readonly Title: string;
 
+  @ApiProperty()
   @IsString()
   readonly Released: string;
 
+  @ApiProperty()
   @IsString()
   readonly Runtime: string;
 
+  @ApiProperty()
   @IsString()
   readonly Genre: string;
 
+  @ApiProperty()
   @IsNumber()
   readonly imdbRating: number;
 }
