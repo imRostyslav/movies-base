c47d760566afa75f9d9cec456c0de8777bceec4e
Rostyslav Lysenko <lysenkor@digicode.com>
2022-05-20 18:13:06 +0300

    add ApiResponse

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,23 +1,31 @@
 import { Controller, Delete, Get, Param, Put, Query, Req } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 import { DeleteResult } from 'typeorm';
 import { GetMovieInfoDto } from './dto/get.Movie.Info.dto';
 import { MovieEntity } from './movie.entity';
 import { MovieService } from './movie.service';
+
 
 @Controller()
 export class MovieController {
   constructor(private readonly movieService: MovieService) { }
 
   @Get('movie')
+  @ApiResponse({ status: 201, description: 'The record has been successfully created.' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   findInfo(@Query() params: GetMovieInfoDto) {
     const movieData = this.movieService.getMovieInfo(params.title);
     return movieData;
   }
   @Get('movies')
+  @ApiResponse({ status: 201, description: 'The data has been successfully received.' })
+  @ApiResponse({ status: 403, description: 'No data.' })
   getAllMovies(): Promise<MovieEntity[]> {
     return this.movieService.getAllMovies();
   }
   @Delete(':id')
+  @ApiResponse({ status: 201, description: 'The movie has been successfully deleted.' })
+  @ApiResponse({ status: 403, description: 'No record with this ID was found' })
   deleteMovieById(@Param('id') id: number): Promise<DeleteResult> {
     return this.movieService.deleteMovieById(id);
   }
--- src/movie/movie.entity.ts
+++ src/movie/movie.entity.ts
@@ -1,22 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'movie' })
 export class MovieEntity {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @Column()
   title: string;
 
+  @ApiProperty()
   @Column({ default: '' })
   released: string;
 
+  @ApiProperty()
   @Column()
   runtime: string;
 
+  @ApiProperty()
   @Column()
   genre: string;
 
+  @ApiProperty()
   @Column()
   imdbRating: number;
 }
